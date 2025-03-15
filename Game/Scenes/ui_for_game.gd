d9e01670d60b4a24728f91bebee914ff71e719a2
BeefyChugass <stefanoswald2002@googlemail.com>
2025-03-15 18:41:20 +0100

    update
    dede

--- Game/Scenes/ui_for_game.gd
+++ Game/Scenes/ui_for_game.gd
@@ -9,8 +9,14 @@ func _ready() -> void:
 func _readyday1():
 	var Line1 = Label.new()
 	var Line2 = Label.new()
+	var Icon1 = Sprite2D.new()
+	var Icon2 = Sprite2D.new()
 	Line1.set_name("Line1")
 	Line2.set_name("Line2")
+	Icon1.texture = load("res://Game/Images/Icons/Ausrufezeichen.png")
+	Icon2.texture = load("res://Game/Images/Icons/Watr.png")
+	get_node("VBoxContainer/Quest1").add_child(Icon1)
+	get_node("VBoxContainer/Quest1").add_child(Icon2)
 	get_node("VBoxContainer/Quest1").add_child(Line1)
 	get_node("VBoxContainer/Quest2").add_child(Line2)
 	Line1.text = "Get new Wood"
