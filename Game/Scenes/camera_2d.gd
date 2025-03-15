31604365002a9819e3e10287d45112f38f7860f8
BeefyChugass <stefanoswald2002@googlemail.com>
2025-03-15 04:06:53 +0100

    Added simple titlescreen stuff
    as said, did :3

---
+++ Game/Scenes/camera_2d.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.global_position.y = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.global_position.y = 0
+
+func _change_parent(node_self):
+	var node = self
+	var new_parent = node_self
+	node.get_parent().remove_child(self)
+	new_parent.add_child(node)
