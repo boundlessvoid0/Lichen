3f86e33c9c2ae24090a02e1e1efca5c78fbfc90e
boundlessvoid <boundlessvoid0@gmail.com>
2025-03-15 14:42:49 +0100

    feat(shaders): removed debugging buttons
    Signed-off-by: boundlessvoid <boundlessvoid0@gmail.com>

--- Game/Scripts/post_process.gd
+++ Game/Scripts/post_process.gd
@@ -2,7 +2,7 @@ extends ColorRect
 
 var _darkness_level : float;
 var _blur_level : float;
-var death: bool;
+var _dead: bool;
 
 func UpdateDarknessLevel(darkness_level) -> void:
 		material.set_shader_parameter("darkness_level", darkness_level);
@@ -13,20 +13,13 @@ func UpdateBlur(blur_level) -> void:
 func SwitchShader(shader : String) -> void:
 	material.shader = load(shader);
 
-func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_D):
-		_darkness_level += delta;
-	if Input.is_key_pressed(KEY_A):
-		_darkness_level += delta;
-	if Input.is_key_pressed(KEY_S):
-		SwitchShader("res://Game/Shaders/death_darken_screen.gdshader");
-	if Input.is_key_pressed(KEY_W):
-		SwitchShader("res://Game/Shaders/darken_screen.gdshader");
-	if Input.is_key_pressed(KEY_K):
-		death = true;
-	if death:
-		SwitchShader("res://Game/Shaders/death_darken_screen.gdshader");
-		_darkness_level += delta * 2;
-		_blur_level += delta * 3;
-		UpdateBlur(_blur_level);
+func death(delta : float) -> void:
+	SwitchShader("res://Game/Shaders/death_darken_screen.gdshader");
+	_darkness_level += delta * 2;
+	_blur_level += delta * 3;
+	UpdateBlur(_blur_level);
 	UpdateDarknessLevel(_darkness_level);
+
+func _process(delta: float) -> void:
+	if _dead:
+		death(delta);
