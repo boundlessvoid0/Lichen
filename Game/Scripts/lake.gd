dd751174e8eafa1215f0ed7548b1a346d1016736
boundlessvoid <boundlessvoid0@gmail.com>
2025-03-16 13:51:14 +0100

    feat(scene): add lake 3
    Signed-off-by: boundlessvoid <boundlessvoid0@gmail.com>

--- Game/Scripts/lake.gd
+++ Game/Scripts/lake.gd
@@ -6,8 +6,10 @@ func GetSequence() -> String:
 	return "Wasser"
 
 func Interact() -> void:
+	$Lake_Area/Lake_Collision.disabled = true
 	return
 
 func Update() -> void:
 	state += 1;
 	self.get_node("Lake_Area/Lake_Sprite").play(str(state));
+	$Lake_Area/Lake_Collision.disabled = false;
