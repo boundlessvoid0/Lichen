3b06be7eb0c31385b49d000bcd5e91819cf587e6
steffler1 <stesi@freenet.de>
2025-03-16 15:46:38 +0100

    sound lake

--- Game/Scripts/lake.gd
+++ Game/Scripts/lake.gd
@@ -3,11 +3,13 @@ extends Node2D
 var state = 0;
 var water_sound = preload("res://Game/Sounds/WasserImEimer.wav")
 
+func _ready():
+	$Crickets_Sound.play()
+
 func GetSequence() -> String:
 	return "Wasser"
 
 func Interact() -> void:
-
 	$AudioStreamPlayer2D.stream = water_sound
 	$AudioStreamPlayer2D.play()
 
@@ -19,3 +21,10 @@ func Update() -> void:
 	state += 1;
 	self.get_node("Lake_Area/Lake_Sprite").play(str(state));
 	$Lake_Area/Lake_Collision.disabled = false;
+
+
+func _on_crickets_sound_finished() -> void:
+	if state < 3:
+		$Crickets_Sound.play()
+	else:
+		$Crickets_Sound.stop()
